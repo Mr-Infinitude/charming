b73bca3bc7bbbb63bca3d6d373637f94a988f7d9
Mr-Infinitude <123817822+Mr-Infinitude@users.noreply.github.com>
2023-02-04 20:42:53 +0530

    Add files via upload

---
+++ bmep.js
@@ -0,0 +1,39 @@
+document.addEventListener("click",function (e)
+{
+    let handle;
+    if(e.target.matches(".handle"))
+    {
+        handle=e.target;
+    }
+    else
+    {
+        handle=e.target.closest(".handle")
+    }
+    if(handle!= null) onHandleClick(handle)
+})
+function onHandleClick(handle)
+{
+    const slider = handle.closest(".container").querySelector(".slider")
+    const sliderIndex=parseInt(getComputedStyle(slider).getPropertyValue("--slider-index")
+    )
+    if(handle.classList.contains("left-handle"))
+   { if(sliderIndex==0)
+    {
+        slider.style.setProperty("--slider-index",0)
+    }
+    else
+    {
+        slider.style.setProperty("--slider-index",sliderIndex -1 )
+    }
+   }
+    if(handle.classList.contains("right-handle"))
+    {
+        if(sliderIndex>10)
+{
+    slider.style.setProperty("--slider-index",0)
+}
+    else{
+        slider.style.setProperty("--slider-index",sliderIndex + 1 )
+    }
+    }
+}
